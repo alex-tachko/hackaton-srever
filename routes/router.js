c233143f4dd37bfb1eb409886549643ac07d4bfc
Aliaksandr Tachko <aliaksandr.tachko@gmail.com>
2022-01-27 16:56:56 +0300

    feat: parse incoming file

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+var http = require('http');
+var XLSX = require('xlsx');
+var formidable = require('formidable');
+
 /**
  * HTTP Status Codes: https://developer.mozilla.org/de/docs/Web/HTTP/Status
  */
@@ -63,11 +67,34 @@ router.get('/shareholders-xls', (req, res, next) => {
 });
 
 router.post('/calculate-preview', (req, res, next) => {
-    const parsedFile = [
-        ['Name', 'Alex', 'Gleb'],
-        ['e-mail address', 'alex@wevestr.com', 'gleb@wevestr.com'],
-    ];
-    res.status(200).send(dataService.getPreview(parsedFile));
+    const form = formidable({ multiples: true });
+    const parsedData = [];
+    form.parse(req, (err, fields, files, next) => {
+        var f = files[Object.keys(files)[0]];
+        var workbook = XLSX.readFile(f.path);
+        const data = XLSX.utils.sheet_to_json(
+            workbook.Sheets[workbook.SheetNames[0]],
+            { header: 1 }
+        );
+        const parsedHeaders = data[0];
+        data.forEach((value, index) => {
+            if (index > 0) {
+                const tempObject = {};
+                parsedHeaders.forEach(
+                    (header, index) => (tempObject[header] = value[index])
+                );
+                parsedData.push(tempObject);
+            }
+        });
+
+        const preview = {
+            existingHeaders: dataService.getHeaders(),
+            parsedTable: parsedData,
+            parsedHeaders,
+        };
+
+        res.status(200).send(preview);
+    });
 });
 
 router.post('/update-table', (req, res, next) => {
