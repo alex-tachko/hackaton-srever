0e3851e9b103efee3850898b1a112b0dc7a678ff
Aliaksandr Tachko <aliaksandr.tachko@gmail.com>
2022-01-27 14:40:14 +0300

    feat: initial service structure

--- routes/router.js
+++ routes/router.js
@@ -12,25 +12,67 @@ router.use('*', (req, res, next) => {
 });
 
 const existingData = {
-    headers: ['User', 'Email'],
-    table: [
-        ['User', 'Mikky'],
-        ['Email', 'mikky@wevestr.com'],
-    ],
+    headers: ['User', 'Email'], // keys of shareholder object values
+    shareholders: [{ id: 1, User: 'Mikky', Email: 'mikky@wevestr.com' }],
 };
 
-router.get('/users', (req, res, next) => {
-    res.status(200).send(existingData.table);
+class DataService {
+    data = {
+        headers: ['User', 'Email'], // keys of shareholder object values
+        shareholders: [{ id: 1, User: 'Mikky', Email: 'mikky@wevestr.com' }],
+    };
+
+    getShareholderById(idToFind) {
+        return this.data.shareholders.find(({ id }) => id === idToFind);
+    }
+
+    getShareholders() {
+        return this.data.shareholders; // possibly format?
+    }
+
+    getHeaders() {
+        return this.data.headers; // possibly format?
+    }
+
+    getShareholdersXls() {
+        return this.data.shareholders; // transform to xls
+    }
+
+    updateShareholders() {}
+
+    getPreview(parsedFile) {
+        return {
+            existingHeaders: this.getHeaders(), // TODO To be changed
+            parsedTable: parsedFile, // TODO To be changed
+        };
+    }
+}
+
+const dataService = new DataService();
+
+router.get('/shareholders/:id', (req, res, next) => {
+    res.status(200).send(dataService.getShareholderById(req.id));
+});
+
+router.get('/shareholders', (req, res, next) => {
+    res.status(200).send(dataService.getShareholders());
+});
+
+router.get('/shareholders-xls', (req, res, next) => {
+    res.status(200).send(dataService.getShareholdersXls()); // TODO send the file
 });
 
 router.post('/calculate-preview', (req, res, next) => {
-    res.status(200).send({
-        existingHeaders: existingData.headers, // TODO To be changed
-        parsedTable: [
-            ['Name', 'Alex', 'Gleb'],
-            ['e-mail address', 'alex@wevestr.com', 'gleb@wevestr.com'],
-        ], // TODO To be changed
-    });
+    const parsedFile = [
+        ['Name', 'Alex', 'Gleb'],
+        ['e-mail address', 'alex@wevestr.com', 'gleb@wevestr.com'],
+    ];
+    res.status(200).send(dataService.getPreview(parsedFile));
+});
+
+router.post('/update-table', (req, res, next) => {
+    dataService.updateShareholders();
+    res.status(200).send(dataService.getShareholders());
 });
 
 module.exports = router;
